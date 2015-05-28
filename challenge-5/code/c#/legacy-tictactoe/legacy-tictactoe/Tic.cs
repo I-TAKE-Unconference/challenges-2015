df131dd3b241e41a671cffe4b38a22972ae77d39
silviue <eigel.si@gmail.com>
2015-05-28 19:49:57 +0300

    extracted (removed duplication) of player move reads

--- challenge-5/code/c#/legacy-tictactoe/legacy-tictactoe/Tic.cs
+++ challenge-5/code/c#/legacy-tictactoe/legacy-tictactoe/Tic.cs
@@ -3,75 +3,95 @@ using System.IO;
 
 namespace legacytictactoe
 {
-	public class Tic
-	{
-		int i, a;
-	    public char[] tab = new char[10];
-	    private TextReader inputStream;
-	    private TextWriter outStream;
+    public class Tic
+    {
+        private int i, a;
+        public char[] tab = new char[10];
+        private readonly TextReader inputStream;
+        private readonly TextWriter outStream;
 
-	    public Tic(TextReader input, TextWriter output)
-	    {
-	        inputStream = input;
-	        outStream = output;
-	    }
+        public Tic(TextReader input, TextWriter output)
+        {
+            inputStream = input;
+            outStream = output;
+        }
 
-	    private void Choice(){
-			double tirage = 0;
-			Random random = new Random((int) DateTime.Now.Ticks & 0x0000FFFF);
-			tirage = random.NextDouble();
+        private void Choice()
+        {
+            double tirage = 0;
+            var random = new Random((int) DateTime.Now.Ticks & 0x0000FFFF);
+            tirage = random.NextDouble();
 
-			if (tirage < 0.5) {
-				for (i = 1; i <= 9; i++)
-				{
-				    if (i % 2 != 0) {
-						outStream.Write("player A:");
-						a = int.Parse(inputStream.ReadLine());
-						tab[a] = 'x';
-					} else {
-						outStream.Write("player B:");
-						a = int.Parse(inputStream.ReadLine());
-						tab[a] = 'o';
-					}
-				}
-			}
+            if (tirage < 0.5)
+            {
+                for (i = 1; i <= 9; i++)
+                {
+                    if (i%2 != 0)
+                    {
+                        ReadPlayerAMove();
+                    }
+                    else
+                    {
+                        ReadPlayerBMove();
+                    }
+                }
+            }
 
-			if (tirage >= 0.5) {
-				for (i = 1; i <= 9; i++) {
-					if (i % 2 != 0) {
-						outStream.Write("player B:");
-						a = int.Parse(inputStream.ReadLine());
-						tab[a] = 'o';
-					} else {
-						outStream.Write("player A:");
-						a = int.Parse(inputStream.ReadLine());
-						tab[a] = 'x';
-					}
-				}
-			}
-		}
+            if (tirage >= 0.5)
+            {
+                for (i = 1; i <= 9; i++)
+                {
+                    if (i%2 != 0)
+                    {
+                        ReadPlayerBMove();
+                    }
+                    else
+                    {
+                        ReadPlayerAMove();
+                    }
+                }
+            }
+        }
 
-		public void Evaluate() {
-			Choice();
-			if (HasPlayerWon('x'))
+        private void ReadPlayerAMove()
+        {
+            outStream.Write("player A:");
+            a = int.Parse(inputStream.ReadLine());
+            tab[a] = 'x';
+        }
 
-				outStream.WriteLine("\nthe winner is : player A\n");
+        private void ReadPlayerBMove()
+        {
+            outStream.Write("player B:");
+            a = int.Parse(inputStream.ReadLine());
+            tab[a] = 'o';
+        }
 
-			if (HasPlayerWon('o'))
+        public void Evaluate()
+        {
+            //SRP: choice, evaluate, output
+            Choice();
+            if (HasPlayerWon('x'))
+            {
+                outStream.WriteLine("\nthe winner is : player A\n");
+            }
 
-				outStream.WriteLine("\nthe winner is : player B\n");
-		}
+            if (HasPlayerWon('o'))
+            {
+                outStream.WriteLine("\nthe winner is : player B\n");
+            }
+        }
 
-	    private bool HasPlayerWon(char playerToken)
-	    {
-	        return (tab[1] == playerToken) && (tab[2] == playerToken) && (tab[3] == playerToken) ||
-	               (tab[4] == playerToken) && (tab[5] == playerToken) && (tab[6] == playerToken) ||
-	               (tab[7] == playerToken) && (tab[8] == playerToken) && (tab[9] == playerToken) ||
-	               (tab[1] == playerToken) && (tab[4] == playerToken) && (tab[7] == playerToken) ||
-	               (tab[2] == playerToken) && (tab[5] == playerToken) && (tab[8] == playerToken) ||
-	               (tab[3] == playerToken) && (tab[6] == playerToken) && (tab[9] == playerToken) ||
-	               (tab[1] == playerToken) && (tab[5] == playerToken) && (tab[9] == playerToken) ||
-	               (tab[3] == playerToken) && (tab[5] == playerToken) && (tab[7] == playerToken);
-	    }
-	}
+        private bool HasPlayerWon(char playerToken)
+        {
+            return (tab[1] == playerToken) && (tab[2] == playerToken) && (tab[3] == playerToken) ||
+                   (tab[4] == playerToken) && (tab[5] == playerToken) && (tab[6] == playerToken) ||
+                   (tab[7] == playerToken) && (tab[8] == playerToken) && (tab[9] == playerToken) ||
+                   (tab[1] == playerToken) && (tab[4] == playerToken) && (tab[7] == playerToken) ||
+                   (tab[2] == playerToken) && (tab[5] == playerToken) && (tab[8] == playerToken) ||
+                   (tab[3] == playerToken) && (tab[6] == playerToken) && (tab[9] == playerToken) ||
+                   (tab[1] == playerToken) && (tab[5] == playerToken) && (tab[9] == playerToken) ||
+                   (tab[3] == playerToken) && (tab[5] == playerToken) && (tab[7] == playerToken);
+        }
+    }
 }
