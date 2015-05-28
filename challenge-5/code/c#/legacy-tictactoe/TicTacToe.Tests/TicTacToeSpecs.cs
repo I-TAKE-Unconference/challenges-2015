4e9ba92ed23c8744449e08ed73bce9e2c1655cdb
silviue <eigel.si@gmail.com>
2015-05-28 19:39:13 +0300

    added wrong input tests

--- challenge-5/code/c#/legacy-tictactoe/TicTacToe.Tests/TicTacToeSpecs.cs
+++ challenge-5/code/c#/legacy-tictactoe/TicTacToe.Tests/TicTacToeSpecs.cs
@@ -18,7 +18,67 @@ namespace TicTacToe.Tests
         }
 
         [TestFixture]
-        public class GameIsADraw:TicTacToeSpecs
+        public class WrongInput : TicTacToeSpecs
+        {
+            [SetUp]
+            public virtual void SetUp()
+            {
+                inputStream = Substitute.For<TextReader>();
+                outputStream = Substitute.For<TextWriter>();
+            }
+
+            [Test]
+            [ExpectedException(typeof (FormatException))]
+            public void FailsForLetters()
+            {
+                var sut = CreateSUT();
+                inputStream.ReadLine().Returns("a");
+
+                sut.eval();
+            }
+
+            [Test]
+            [ExpectedException(typeof (FormatException))]
+            public void FailsForSpecialCharacters()
+            {
+                var sut = CreateSUT();
+                inputStream.ReadLine().Returns(".");
+
+                sut.eval();
+            }
+            [Test]
+            [ExpectedException(typeof(IndexOutOfRangeException))]
+            public void FailsForSmallerNumbers()
+            {
+                var sut = CreateSUT();
+                inputStream.ReadLine().Returns("-1");
+
+                sut.eval();
+            }          
+            
+            [Test]
+            [ExpectedException(typeof(IndexOutOfRangeException))]
+            public void FailsForBiggerNumbers()
+            {
+                var sut = CreateSUT();
+                inputStream.ReadLine().Returns("10");
+
+                sut.eval();
+            }
+
+            [Test]
+            public void ZeroPositionIsAccepted()
+            {
+                var sut = CreateSUT();
+                inputStream.ReadLine().Returns("0", "3", "2", "4", "6", "5", "7", "8", "9");
+
+                sut.eval();
+            }
+        }
+
+
+        [TestFixture]
+        public class GameIsADraw : TicTacToeSpecs
         {
             [SetUp]
             public virtual void SetUp()
@@ -34,10 +94,9 @@ namespace TicTacToe.Tests
                 inputStream.ReadLine().Returns("1", "3", "2", "4", "6", "5", "7", "8", "9");
 
                 sut.eval();
-                outputStream.Received(0)
-                    .WriteLine(Arg.Is((string str) => str.Contains("the winner is")));
-                outputStream.Write(Arg.Any<string>());
+                outputStream.Received(0).WriteLine(Arg.Is((string str) => str.Contains("the winner is")));
             }
+
             [Test]
             public void NoOneWins_ForSameRepeatedPosition()
             {
@@ -45,12 +104,8 @@ namespace TicTacToe.Tests
                 inputStream.ReadLine().Returns("1", "1", "1", "1", "1", "1", "1", "1", "1");
 
                 sut.eval();
-                outputStream.Received(0)
-                    .WriteLine(Arg.Is((string str) => str.Contains("the winner is")));
-                outputStream.Write(Arg.Any<string>());
+                outputStream.Received(0).WriteLine(Arg.Is((string str) => str.Contains("the winner is")));
             }
-
-
         }
 
         [TestFixture]
@@ -82,9 +137,7 @@ namespace TicTacToe.Tests
                 inputStream.ReadLine().Returns("1", "3", "5", "6", "9", "2", "4", "7", "8");
 
                 sut.eval();
-                outputStream.Received()
-                    .WriteLine(Arg.Is((string str) => str.Equals(WinningPlayerMessage())));
-                outputStream.Write(Arg.Any<string>());
+                outputStream.Received().WriteLine(Arg.Is((string str) => str.Equals(WinningPlayerMessage())));
             }
 
             [Test]
@@ -93,8 +146,7 @@ namespace TicTacToe.Tests
                 var sut = CreateSUT();
                 inputStream.ReadLine().Returns("3", "2", "5", "1", "7", "9", "4", "6", "8");
                 sut.eval();
-                outputStream.Received()
-                    .WriteLine(Arg.Is((string str) => str.Equals(WinningPlayerMessage())));
+                outputStream.Received().WriteLine(Arg.Is((string str) => str.Equals(WinningPlayerMessage())));
             }
 
             [Test]
@@ -103,8 +155,7 @@ namespace TicTacToe.Tests
                 var sut = CreateSUT();
                 inputStream.ReadLine().Returns("1", "4", "2", "6", "3", "5", "9", "7", "8");
                 sut.eval();
-                outputStream.Received()
-                    .WriteLine(Arg.Is((string str) => str.Equals(WinningPlayerMessage())));
+                outputStream.Received().WriteLine(Arg.Is((string str) => str.Equals(WinningPlayerMessage())));
             }
         }
 
@@ -126,7 +177,7 @@ namespace TicTacToe.Tests
 
             private string WinningPlayerMessage()
             {
-                return string.Format("\nthe winner is : player {0}\n" , GetSecondPlayerName());
+                return string.Format("\nthe winner is : player {0}\n", GetSecondPlayerName());
             }
 
             [Test]
@@ -135,8 +186,7 @@ namespace TicTacToe.Tests
                 var sut = CreateSUT();
                 inputStream.ReadLine().Returns("8", "1", "3", "5", "6", "9", "2", "4", "7");
                 sut.eval();
-                outputStream.Received()
-                    .WriteLine(Arg.Is((string str) => str.Equals(WinningPlayerMessage())));
+                outputStream.Received().WriteLine(Arg.Is((string str) => str.Equals(WinningPlayerMessage())));
             }
 
             [Test]
@@ -145,8 +195,7 @@ namespace TicTacToe.Tests
                 var sut = CreateSUT();
                 inputStream.ReadLine().Returns("8", "3", "2", "5", "1", "7", "9", "4", "6", "8");
                 sut.eval();
-                outputStream.Received()
-                    .WriteLine(Arg.Is((string str) => str.Equals(WinningPlayerMessage())));
+                outputStream.Received().WriteLine(Arg.Is((string str) => str.Equals(WinningPlayerMessage())));
             }
 
             [Test]
@@ -155,8 +204,7 @@ namespace TicTacToe.Tests
                 var sut = CreateSUT();
                 inputStream.ReadLine().Returns("8", "1", "4", "2", "6", "3", "5", "9", "7", "8");
                 sut.eval();
-                outputStream.Received(1)
-                    .WriteLine(Arg.Is((string str) => str.Equals(WinningPlayerMessage())));
+                outputStream.Received(1).WriteLine(Arg.Is((string str) => str.Equals(WinningPlayerMessage())));
                 Console.Out.WriteLine("WinningPlayerMessage = {0}", WinningPlayerMessage());
             }
         }
