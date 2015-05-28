94b0206ee98fffa58c319c371c10127cc499cbea
silviue <eigel.si@gmail.com>
2015-05-28 18:40:24 +0300

    added tests for firsta player winning by first line and second diagonal;added tests for second player

--- challenge-5/code/c#/legacy-tictactoe/TicTacToe.Tests/TicTacToeSpecs.cs
+++ challenge-5/code/c#/legacy-tictactoe/TicTacToe.Tests/TicTacToeSpecs.cs
@@ -34,6 +34,62 @@ namespace TicTacToe.Tests
                 sut.eval();
                 outputStream.Received().WriteLine(Arg.Is((string str) => str.Contains("the winner is :")));
             }
+
+            [Test]
+            public void RightToLeftDiagonal()
+            {
+                var sut = CreateSUT();
+                inputStream.ReadLine().Returns("3", "2", "5", "1", "7", "9", "4", "6", "8");
+                sut.eval();
+                outputStream.Received().WriteLine(Arg.Is((string str) => str.Contains("the winner is :")));
+            }
+
+            [Test]
+            public void LeftToRightFirstLine()
+            {
+                var sut = CreateSUT();
+                inputStream.ReadLine().Returns("1", "4", "2", "6", "3", "5", "9", "7", "8");
+                sut.eval();
+                outputStream.Received().WriteLine(Arg.Is((string str) => str.Contains("the winner is :")));
+            }
+        }
+
+        [TestFixture]
+        public class SecondPlayerWins : TicTacToeSpecs
+        {
+            [SetUp]
+            public virtual void SetUp()
+            {
+                inputStream = Substitute.For<TextReader>();
+                outputStream = Substitute.For<TextWriter>();
+            }
+
+            [Test]
+            public void LeftToRightDiagonal()
+            {
+                var sut = CreateSUT();
+                inputStream.ReadLine().Returns("8", "1", "3", "5", "6", "9", "2", "4", "7");
+                sut.eval();
+                outputStream.Received().WriteLine(Arg.Is((string str) => str.Contains("the winner is :")));
+            }
+
+            [Test]
+            public void RightToLeftDiagonal()
+            {
+                var sut = CreateSUT();
+                inputStream.ReadLine().Returns("8", "3", "2", "5", "1", "7", "9", "4", "6", "8");
+                sut.eval();
+                outputStream.Received().WriteLine(Arg.Is((string str) => str.Contains("the winner is :")));
+            }
+
+            [Test]
+            public void LeftToRightFirstLine()
+            {
+                var sut = CreateSUT();
+                inputStream.ReadLine().Returns("8", "1", "4", "2", "6", "3", "5", "9", "7", "8");
+                sut.eval();
+                outputStream.Received().WriteLine(Arg.Is((string str) => str.Contains("the winner is :")));
+            }
         }
     }
 }
