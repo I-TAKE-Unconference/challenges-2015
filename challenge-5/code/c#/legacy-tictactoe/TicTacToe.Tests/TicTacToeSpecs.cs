bdf83a5f4b19f79ae7a7b40fad4ba58b179f3c1f
silviue <eigel.si@gmail.com>
2015-05-28 17:47:03 +0300

    added first visual test (didn't isolate from Console.WriteLing)

---
+++ challenge-5/code/c#/legacy-tictactoe/TicTacToe.Tests/TicTacToeSpecs.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using legacytictactoe;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace TicTacToe.Tests
+{
+    public class TicTacToeSpecs
+    {
+        public Tic CreateSUT()
+        {
+            var inputStream = Substitute.For<TextReader>();
+
+            inputStream.ReadLine().Returns("1", "3", "5", "6", "9", "2", "4", "7", "8");
+            return new Tic(inputStream);
+        }
+
+
+        [TestFixture]
+        public class When_TicTacToeSpecs : TicTacToeSpecs
+        {
+            [Test]
+            public void Should_Behavior()
+            {
+                var sut = CreateSUT();
+                sut.eval();
+            }
+        }
+    }
+}
