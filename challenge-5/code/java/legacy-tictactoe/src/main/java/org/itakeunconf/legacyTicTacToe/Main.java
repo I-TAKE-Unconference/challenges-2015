7d3a08b828ad3b18d2cae67d61bd8226b9b68884
liviuE <liviu_esanu@yahoo.com>
2015-05-29 06:48:38 +0200

    Update Main.java

--- challenge-5/code/java/legacy-tictactoe/src/main/java/org/itakeunconf/legacyTicTacToe/Main.java
+++ challenge-5/code/java/legacy-tictactoe/src/main/java/org/itakeunconf/legacyTicTacToe/Main.java
@@ -3,17 +3,15 @@ package org.itakeunconf.legacyTicTacToe;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Tic tic = new Tic();
-        try {
-            tic.eval();
-            for (int i = 1; i <= 9; i++) {
-                System.out.print(tic.tab[i]);
-                if (i == 3 || i == 6 || i == 9)
-                    System.out.print("\n");
-            }
-        } catch (IOException exc){
-            exc.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+		Tic tic = new Tic(3);
+
+		try {
+			tic.eval();
+		} catch (IOException exc) {
+			exc.printStackTrace();
+		}
+		
+		tic.printTable(System.out);
+	}
 }
