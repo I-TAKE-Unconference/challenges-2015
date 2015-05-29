e9ec0d416c88e5753c932fd85551b1140fa01d65
markus <liviu_esanu@yahoo.com>
2015-05-29 07:27:11 +0200

    challenge 5

--- challenge-5/code/java/legacy-tictactoe/src/main/java/org/itakeunconf/legacyTicTacToe/Main.java
+++ challenge-5/code/java/legacy-tictactoe/src/main/java/org/itakeunconf/legacyTicTacToe/Main.java
@@ -3,17 +3,23 @@ package org.itakeunconf.legacyTicTacToe;
 import java.io.IOException;
 
 public class Main {
+    private static final TABLE_CELL_COUNT = 9;
+
     public static void main(String[] args) {
         Tic tic = new Tic();
+
         try {
             tic.eval();
-            for (int i = 1; i <= 9; i++) {
+         
+	   for (int i = 1; i <= TABLE_CELL_COUNT; i++) {
                 System.out.print(tic.tab[i]);
-                if (i == 3 || i == 6 || i == 9)
+                
+		if (i % 3 == 0)
                     System.out.print("\n");
             }
         } catch (IOException exc){
             exc.printStackTrace();
         }
     }
+
 }
