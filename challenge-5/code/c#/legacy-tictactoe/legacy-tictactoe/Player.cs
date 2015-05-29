ed7bb870778c0ebcfacc3978beb4363e26c2bd5a
Liviu Costea <email.lcostea@gmail.com>
2015-05-29 13:15:11 +0300

    refactoring challange 5
    Marius Mutescu & Liviu Costea

---
+++ challenge-5/code/c#/legacy-tictactoe/legacy-tictactoe/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace legacytictactoe
+{
+    public sealed class Player
+    {
+        public static readonly Player A = new Player("Player A", 'x');
+        public static readonly Player B = new Player("Player B", 'o');
+
+        public readonly string PlayerName;
+        public readonly char PlayerCharacter; // x or o
+
+        private Player(string name, char playerCharacter)
+        {
+            PlayerName = name;
+            PlayerCharacter = playerCharacter;
+        }
+        public static Player GetPlayerByPlayingCharacter(char playingCharacter)
+        {
+            return playingCharacter == 'x' ? Player.A : Player.B;
+        }
+    }
+}
---
+++ challenge-5/code/c#/legacy-tictactoe/legacy-tictactoe/Winner.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace legacytictactoe
+{
+    internal class WinnerService
+    {
+        public static void CheckWinner(Player player, char[] tab)
+        {
+            bool winner = LineWinner(player, tab) || ColumnWinner(player, tab) || DiagonalWinner(player, tab);
+            if (winner)
+            {
+                Console.WriteLine("\nThe winner is : " + player.PlayerName);
+            }
+        }
+        private static bool LineWinner(Player player, char[] tab)
+        {
+            int position = 0;
+            while (position < 9)
+            {
+                if (tab[position + 1] == tab[position + 2] && tab[position + 2] == tab[position + 3] && tab[position + 1] == player.PlayerCharacter)
+                {
+                    return true;
+                }
+                position += 3;
+            }
+            return false;
+        }
+        private static bool ColumnWinner(Player player, char[] tab)
+        {
+            int position = 0;
+            while (position < 3)
+            {
+                if (tab[position + 1] == tab[position + 4] && tab[position + 4] == tab[position + 7] && tab[position + 1] == player.PlayerCharacter)
+                {
+                    return true;
+                }
+                position += 1;
+            }
+            return false;
+        }
+        private static bool DiagonalWinner(Player player, char[] tab)
+        {
+            if (tab[1] == tab[5] && tab[5] == tab[9] && tab[1] == player.PlayerCharacter)
+            {
+                return true; ;
+            }
+            if (tab[3] == tab[5] && tab[5] == tab[7] && tab[3] == player.PlayerCharacter)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
