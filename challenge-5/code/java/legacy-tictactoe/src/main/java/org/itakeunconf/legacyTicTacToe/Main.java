adb1512ee7d8f4b02e6e780c8d862bf60fe471f6
Paul <paul@Paul-Bs-MacBook-Pro.local>
2015-05-29 13:19:58 +0300

    change also the main.java

--- challenge-5/code/java/legacy-tictactoe/src/main/java/org/itakeunconf/legacyTicTacToe/Main.java
+++ challenge-5/code/java/legacy-tictactoe/src/main/java/org/itakeunconf/legacyTicTacToe/Main.java
@@ -7,11 +7,6 @@ public class Main {
         Tic tic = new Tic();
         try {
             tic.eval();
-            for (int i = 1; i <= 9; i++) {
-                System.out.print(tic.tab[i]);
-                if (i == 3 || i == 6 || i == 9)
-                    System.out.print("\n");
-            }
         } catch (IOException exc){
             exc.printStackTrace();
         }
